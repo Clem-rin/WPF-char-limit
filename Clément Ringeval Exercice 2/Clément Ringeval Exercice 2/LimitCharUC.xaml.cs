20a76d2e72056c9493d38ca61030e26da06c6475
Clem-rin <72383804+Clem-rin@users.noreply.github.com>
2021-01-30 11:30:22 +0100

    Add files via upload

---
+++ Clément Ringeval Exercice 2/Clément Ringeval Exercice 2/LimitCharUC.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clément_Ringeval_Exercice_2
+{
+    /// <summary>
+    /// Logique d'interaction pour LimitCharUC.xaml
+    /// </summary>
+    public partial class LimitCharUC : UserControl
+    {
+        public LimitCharUC(string titre, int maxlength)
+        {
+            InitializeComponent();
+            titreUC.Content = titre;
+            maxLength.Content += maxlength.ToString();
+            entree.MaxLength = maxlength;
+        }
+    }
+}
---
+++ Clément Ringeval Exercice 2/Clément Ringeval Exercice 2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Clément_Ringeval_Exercice_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void TextBoxGotKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            TextBox source = e.Source as TextBox;
+
+            if (source != null)
+            {
+                source.Clear();
+                source.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                LimitCharUC nvControle = new LimitCharUC(titre.Text, int.Parse(maxlength.Text));
+                this.w1.Children.Add(nvControle);
+                titre.FontStyle = FontStyles.Italic;
+                maxlength.FontStyle = FontStyles.Italic;
+                titre.Text = "Titre";
+                maxlength.Text = "MaxLength";
+            }
+            catch
+            {
+                MessageBox.Show("MaxLength doit être un int");
+            }
+        }
+    }
+}
